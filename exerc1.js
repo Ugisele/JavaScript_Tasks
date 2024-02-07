9490060744701ab95acf0b9ff8b23c1a41e818e6
Ugisele <Umutonigisele12@gmail.com>
2024-02-07 10:28:45 +0200

    map,pushpop and reduce objects in js

--- exerc1.js
+++ exerc1.js
@@ -1,13 +1,13 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
 // console.log(colors);
 // Using the join() method,
-// const joinIndex = colors.join(',');
-// console.log(joinIndex);
+const joinIndex = colors.join(',');
+console.log(joinIndex);
 
 /*Apply the splice() method to remove the second and third elements from 
 the colors array and replace them with 'purple' and 'pink'.*/
 colors.splice(2, 2, "purple","pink")
-console.log(colors);
+// console.log(colors);
 //Utilize the copyWithin() method to copy the elements from index 1 to index 3 within the colors array to index 0.
 colors.copyWithin(0,1,3);
 // console.log(colors);
